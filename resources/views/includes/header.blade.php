bc48ba0fff4f42da2818e5b02231f4364a899fa5
AndreaChiari <chiariandrea94@gmail.com>
2023-03-03 14:30:49 +0100

    add-header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">TOOLS</a>
+        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">ABOUT US</a>
+        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">CONTACT US</a>
+        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">PRIVACY POLICY</a>
+      </div>
+    </nav>
+  </header>
