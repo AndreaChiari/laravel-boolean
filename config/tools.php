3fd41254c1f38a0b828ea0e4e6ca0079bf13878b
AndreaChiari <chiariandrea94@gmail.com>
2023-03-03 12:50:50 +0100

    add-boiler-plate-add-dbcreated

---
+++ config/tools.php
@@ -0,0 +1,37 @@
+<?php
+return  [
+    [
+        'name' => 'Visual Studio Code',
+        'image_url' => 'https://via.placeholder.com/150',
+        'description' => 'Editor di codice open source',
+        'category' => 'Editor di codice',
+        'release_year' => 2015,
+        'latest_version' => '1.63.2',
+        'download_link' => 'https://code.visualstudio.com/',
+        'supported_os' => 'Windows, macOS, Linux',
+        'vote' => 4.7,
+    ],
+    [
+        'name' => 'Adobe Photoshop',
+        'image_url' => 'https://via.placeholder.com/150',
+        'description' => 'Software di fotoritocco',
+        'category' => 'Grafica',
+        'release_year' => 1990,
+        'latest_version' => '2022',
+        'download_link' => 'https://www.adobe.com/products/photoshop.html',
+        'supported_os' => 'Windows, macOS',
+        'vote' => 4.5,
+    ],
+    [
+        'name' => 'Google Chrome',
+        'image_url' => 'https://via.placeholder.com/150',
+        'description' => 'Browser web sviluppato da Google',
+        'category' => 'Browser web',
+        'release_year' => 2008,
+        'latest_version' => '98.0.4758.102',
+        'download_link' => 'https://www.google.com/chrome/',
+        'supported_os' => 'Windows, macOS, Linux',
+        'vote' => 4.8,
+    ],
+
+];
